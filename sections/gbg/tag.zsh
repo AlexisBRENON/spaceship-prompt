b0b1b03319f3956a21a1ab077fe2810346795da5
Alexis BRENON <alexis.brenon@adyoulike.com>
2019-04-18 16:05:20 +0200

    Fix separator padding

--- sections/gbg/tag.zsh
+++ sections/gbg/tag.zsh
@@ -34,11 +34,13 @@ SPACESHIP_GBG_TAG_SYMBOLS="$(spaceship_gbg_local_init_symbols)\\n"
 # ------------------------------------------------------------------------------
 
 spaceship_gbg_tag() {
+  [[ "${gbg_head_is_on_tag:-false}" = "false" ]] && return
+
   [[ -n "${SPACESHIP_GBG_LAST_BG_COLOR}" ]] && lf_spaceship_gbg_close_segment "${SPACESHIP_GBG_TAG_BG_COLOR}"
   [[ -n "${SPACESHIP_GBG_TAG_FG_COLOR}" ]] && fg_color="%F{${SPACESHIP_GBG_TAG_FG_COLOR}}" || fg_color="%f"
   [[ -n "${SPACESHIP_GBG_TAG_BG_COLOR}" ]] && bg_color="%K{${SPACESHIP_GBG_TAG_BG_COLOR}}" || bg_color="%k"
 
-  l_section_out="%{${bg_color}${fg_color}%}" # Set colors
+  l_section_out="%{${bg_color}${fg_color}%} " # Set colors
 
   l_section_out="${l_section_out}$(\
     lf_spaceship_gbg_enrich_append \
@@ -48,7 +50,7 @@ spaceship_gbg_tag() {
           tag) ${gbg_head_tag:-}"\
     )"
 
-  l_section_out="${l_section_out}%{%f%k%}"
+  l_section_out="${l_section_out} %{%f%k%}"
 
   printf '%s' "${l_section_out}"
   unset l_section_out
